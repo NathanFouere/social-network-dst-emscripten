982074730293525b9430398b58e9c7e45d47233c
Nathan Fouere <nathanfouere@tutanota.com>
2025-10-23 11:34:06 +0200

    complete creator

--- convertedMicroServices/Types/Creator/Creator.cpp
+++ convertedMicroServices/Types/Creator/Creator.cpp
@@ -3,7 +3,26 @@
 
 using namespace emscripten;
 
+int64_t Creator::getUserId() const {
+    return this->user_id;
+}
+
+void Creator::setUserId(int64_t _user_id) {
+    this->user_id = _user_id;
+}
+
+std::string Creator::getUsername() const {
+    return this->username;
+}
+
+void Creator::setUsername(std::string _username) {
+    this->username = _username;
+}
+
 EMSCRIPTEN_BINDINGS(creator_module) {
     class_<Creator>("Creator")
-    .constructor<>();
+        .constructor<>()
+        .property("user_id", &Creator::getUserId, &Creator::setUserId)
+        .property("username", &Creator::getUsername, &Creator::setUsername)
+    ;
 }
